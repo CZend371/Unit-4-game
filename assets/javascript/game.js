15ca5e4a533fddbe292d72aa93f96e899af84ee9
Cristian371 <czend371@gmail.com>
2019-05-04 09:55:35 -0500

    Got crystals to provide value and update a display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
 
-var randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-var randomCrystalValue = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    current = 0;
+    var redNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    var purpleNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    var blueNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    var greenNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
 
-console.log(randomTargetValue);
-$("#crystalimg").click(function() {
-crystal = randomCrystalValue;
-console.log(crystal)
-});
+    resetGame = function() {
+        
+         current = 0;
+        
+    }
+    resetGame()
+
+$("#crystal-red").attr('value', redNumber);
+$("#crystal-blue").attr('value', blueNumber);
+$("#crystal-green").attr('value', greenNumber);
+$("#crystal-purple").attr('value', purpleNumber);
+
+$(".crystal").click(function() {
+    current = (redNumber + blueNumber + greenNumber + purpleNumber);
+    // console.log($(this).attr('value'));
+    $("#counter").text(current);
+})
 })
