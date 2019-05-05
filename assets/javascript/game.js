b2ff362a8beb761fe94c75c62770a359141a169b
Cristian371 <czend371@gmail.com>
2019-05-05 11:08:00 -0500

    added css properties and background img

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,48 @@
 $(document).ready(function() {
 
-    randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    current = 0;
+    var randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     var redNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
     var purpleNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
     var blueNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
     var greenNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
-    wins = 0;
-    losses = 0;
+    var wins = 0;
+    var losses = 0;
+    var current = 0;
 
-$("#crystal-red").attr('value', redNumber);
-$("#crystal-blue").attr('value', blueNumber);
-$("#crystal-green").attr('value', greenNumber);
-$("#crystal-purple").attr('value', purpleNumber);
+    $("#crystal-red").attr('value', redNumber);
+    $("#crystal-blue").attr('value', blueNumber);
+    $("#crystal-green").attr('value', greenNumber);
+    $("#crystal-purple").attr('value', purpleNumber);
 
-$("#goal").text(randomTargetValue);
+    $("#goal").text(" " + randomTargetValue);
+    $("#wins").text(" " + wins);
+    $("#losses").text(" " + losses);
 
-$(".crystal").click(function() {
+    $(".crystal").click(function() {
     current += parseInt($(this).attr('value'));
     console.log($(this).attr('value'));
-    $("#counter").text(current);
-})
-updateDisplay = function() {
-    if(current === randomTargetValue) {
-        $("wins").text(wins);
-        wins++
-        resetGame();
-}else (current > randomTargetValue)
-    $("losses").text(losses);
-    losses++
-    resetGame();
-}
-resetGame = function() {
-    randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    current = 0;
-    redNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
-    purpleNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
-    blueNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
-    greenNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    $("#counter").text(" " + current);
+    })
+    
+    resetGame = function() {
+        randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        current = 0;
+        redNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+        purpleNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+        blueNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+        greenNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
     }
+        // resetGame();
+    
+    updateDisplay = function() {
+        if(current === randomTargetValue) {
+            wins++;
+            resetGame();
+        }else (current > randomTargetValue)
+            losses++;
+            resetGame();
+        }
+    updateDisplay();
+
+    
 })
