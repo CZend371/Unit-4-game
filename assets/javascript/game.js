e4d74dab1e98b4ed85b12ceccb90c189a3779da4
Cristian371 <czend371@gmail.com>
2019-05-04 19:32:36 -0500

    added reset and update functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,22 +6,37 @@ $(document).ready(function() {
     var purpleNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
     var blueNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
     var greenNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
-
-    resetGame = function() {
-        
-         current = 0;
-        
-    }
-    resetGame()
+    wins = 0;
+    losses = 0;
 
 $("#crystal-red").attr('value', redNumber);
 $("#crystal-blue").attr('value', blueNumber);
 $("#crystal-green").attr('value', greenNumber);
 $("#crystal-purple").attr('value', purpleNumber);
 
+$("#goal").text(randomTargetValue);
+
 $(".crystal").click(function() {
-    current = (redNumber + blueNumber + greenNumber + purpleNumber);
-    // console.log($(this).attr('value'));
+    current += parseInt($(this).attr('value'));
+    console.log($(this).attr('value'));
     $("#counter").text(current);
 })
+updateDisplay = function() {
+    if(current === randomTargetValue) {
+        $("wins").text(wins);
+        wins++
+        resetGame();
+}else (current > randomTargetValue)
+    $("losses").text(losses);
+    losses++
+    resetGame();
+}
+resetGame = function() {
+    randomTargetValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    current = 0;
+    redNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    purpleNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    blueNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    greenNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
+    }
 })
